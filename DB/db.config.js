e684506fdc5b32f8e68d75aeb2eebe3ca2206073
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-09-29 17:45:57 -0300

    Migrando mysql a postgresql

--- DB/db.config.js
+++ DB/db.config.js
@@ -1,11 +1,25 @@
-const mysql = require('mysql2');
 const config = require('../config/config.js');
 
-const connection = mysql.createConnection({
-  host: config.host,
-  user: config.username,
-  password: config.password,
-  database: config.database,
-});
+const { Client } = require('pg');
 
-module.exports = connection;
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'api_movies_and_series',
+  password: 'cantera2023',
+  port: 5432,
+});
+client.connect();
+
+const moviesTable = 'CREATE TABLE movies (id SERIAL PRIMARY KEY,title VARCHAR(255), description TEXT, img VARCHAR(255), release_year INTEGER )';
+const seriesTable = 'CREATE TABLE series (id SERIAL PRIMARY KEY,title VARCHAR(255), description TEXT, img VARCHAR(255), release_year INTEGER )';
+
+client.query('SELECT * FROM movies', (err, res)=>{
+  if (!err) {
+    console.log('err no existe');
+    console.log(res);
+  } else {
+    console.error(err);
+  }
+  client.end()
+});
--- config/config.js
+++ config/config.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 const config = {
   host: process.env.HOST,
-  database: process.env.DB,
-  password: process.env.PASSWORD,
-  username: process.env.USERNAME_DB
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  username: process.env.DB_USERNAME,
+  port: process.env.PORT
 };
 
 module.exports = config;
