61184a6619774f76c0eea082940fed8cf9ecb0d2
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-10-02 14:33:04 -0300

    creada la crud para eliminar movies

--- routes/movies-routes.js
+++ routes/movies-routes.js
@@ -50,11 +50,22 @@ router.patch('/:id', async (req, res) => {
     const updateMovieSQL = "UPDATE movies SET title = COALESCE($1, title), description = COALESCE($2, description), img = COALESCE($3, img), release_year = COALESCE($4, release_year) WHERE id = $5";
     const values = [title, description, img, releaseYear, id];
     const results = await queryDatabase(updateMovieSQL, values);
-    res.status(200).json({message: 'Sent Successfully', results});
+    res.status(200).json({ message: 'Sent Successfully', results });
   } catch (error) {
     console.log(error);
     res.status(400).json({ error: 'Consulta inválida' });
   }
 });
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteMovieSQL = `DELETE FROM movies WHERE id = ${id}`
+    const results = await queryDatabase(deleteMovieSQL);
+    res.status(200).json({ message: 'Successfully removed', results })
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+})
 
 module.exports = router;
