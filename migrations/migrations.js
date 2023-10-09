dba3245936c45df0e26374b853fd71eb6d0c8ccc
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-10-09 17:06:24 -0300

    Se crea un cliente que corra las migraciones y luego otro para que corra las consultas

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -1,19 +1,30 @@
 const config = require('../config/config.js');
 const path = require('path');
+const { Client } = require('pg');
 const fs = require('fs');
-const queryDatabase = require('../DB/db.config.js');
 
 const migrationFilePath = path.resolve(__dirname, '001-create-tables.sql');
 const migrationSQL = fs.readFileSync(migrationFilePath, 'utf8');
 
 
-
+const client = new Client({
+  user: config.username,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.dbPort,
+  ssl: false
+});
 
 async function runMigration() {
   try {
-    await queryDatabase(migrationSQL);
+    await client.connect();
+    await client.query(migrationSQL);
+    console.log('Migración exitosa');
   } catch (error) {
     console.error('Error durante la migración:', error);
+  } finally{
+    client.end();
   }
 }
 
