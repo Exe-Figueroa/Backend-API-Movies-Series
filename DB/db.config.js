7bea917af67382b68b945601ee9bd07250970df7
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-10-09 17:15:22 -0300

    Apertura y cierre de sesion por cada consulta

--- DB/db.config.js
+++ DB/db.config.js
@@ -2,22 +2,24 @@ const config = require('../config/config.js');
 
 const { Client } = require('pg');
 
-const connection = new Client({
-  user: config.username,
-  host: config.host,
-  database: config.database,
-  password: config.password,
-  port: config.dbPort,
-  ssl: false
-});
-
 function queryDatabase(query, values) {
+  const connection = new Client({
+    user: config.username,
+    host: config.host,
+    database: config.database,
+    password: config.password,
+    port: config.dbPort,
+    ssl: false
+  });
+
   connection.connect();
   return new Promise((resolve, reject) => {
     connection.query(query, values, (err, result) => {
       if (err) {
+        connection.end();
         reject(err);
       } else {
+        connection.end();
         resolve(result);
       };
     });
