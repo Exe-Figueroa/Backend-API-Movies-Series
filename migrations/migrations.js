027b966b9dfc7bdbf5760270c7beee3addc8176c
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-10-09 15:13:16 -0300

    Creación de archivo para correr migraciones

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -1,5 +1,31 @@
-const queryDatabase = require('../DB/db.config.js');
-const moviesTableQuery = 'CREATE TABLE movies (id serial PRIMARY KEY, title varchar(255), description text,release_year int, img varchar(255), category varchar)';
-const seriesTableQuery = 'CREATE TABLE series (id serial PRIMARY KEY, title varchar(255), description text,release_year int, img varchar(255), category varchar(20))';
-queryDatabase(moviesTableQuery);
-queryDatabase(seriesTableQuery);
+const { config } = require('../config/config.js');
+const { Client } = require('pg');
+const fs = require('fs');
+
+// Lee el contenido del archivo de migración SQL
+const migrationSQL = fs.readFileSync('001-create-tables.sql', 'utf8');
+
+// Configuración de conexión a la base de datos
+const client = new Client({
+  user: config.username,
+  host: config.host,
+  database: config.database,
+  password: config.password,
+  port: config.dbPort,
+});
+
+// Función para ejecutar la migración
+async function runMigration() {
+  try {
+    await client.connect();
+    await client.query(migrationSQL);
+    console.log('Migración exitosa');
+  } catch (error) {
+    console.error('Error durante la migración:', error);
+  } finally {
+    await client.end();
+  }
+}
+
+// Ejecuta la migración
+runMigration();
